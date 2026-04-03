7d001c105eb1a2e419b9762fc65c2dbb612caf4a
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-03 10:46:42 +0200

    implement canUnlockAll function and add main test file

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -8,4 +8,17 @@ def canUnlockAll(boxes):
     Returns:
         True if all boxes can be opened, else False
     """
-    
+    if not isinstance(boxes, list):
+        return False
+    if len(boxes) == 0:
+        return True
+    opened = set()
+    queue = [0]
+    opened.add(0)
+    while queue:
+        box = queue.pop(0)
+        for key in boxes[box]:
+            if key not in opened and key < len(boxes):
+                opened.add(key)
+                queue.append(key)
+    return len(opened) == len(boxes)
