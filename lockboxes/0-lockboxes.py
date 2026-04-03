cb7312da19d308f923876fa05ad8a86f815efb0d
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-03 10:29:10 +0200

    add initial implementation of canUnlockAll function

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""file that contains the function canUnlockAll that determines
+if all the boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    """function that determines if all the boxes can be opened
+    Returns:
+        True if all boxes can be opened, else False
+    """
+    
