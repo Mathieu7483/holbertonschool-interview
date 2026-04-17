954e97c4bf8ca609b8acb31ce3db53323d525ae1
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-17 09:44:29 +0200

    feat: add function prototypes for binary_tree_size and get_parent in binary_trees.h

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -27,5 +27,7 @@ typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
+size_t binary_tree_size(const binary_tree_t *tree);
+heap_t *get_parent(heap_t *root, int size);
 
 #endif /*BINARY_TREES_H*/
