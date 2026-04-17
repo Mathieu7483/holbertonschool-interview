cc69a2fde9ee7d26ff074108fbe6a24c35c065b9
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-17 09:36:42 +0200

    feat: implement heap_insert function to insert values in Max Binary Heap

---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,64 @@
+#include "binary_trees.h"
+
+/**binary_tree_size - function that calculate the size of the tree
+ * @tree: is a pointer to the root node of the tree to measure the size
+ * Return: size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+  if (tree == NULL)
+    return (0);
+  return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+
+/**
+*heap_insert - function that inserts a value in Max Binary Heap
+*@root: is a double pointer to the root node of the Heap
+*@value: is the value to store in the node to be inserted
+*Return: pointer to the created node, or NULL on failure
+*/
+heap_t *heap_insert(heap_t **root, int value)
+{
+  heap_t *new_node, *parent;
+  int size, mask, temp;
+
+  if (root == NULL)
+    return (NULL);
+
+  if (*root == NULL)
+    return (*root = binary_tree_node(NULL, value));
+
+  size = binary_tree_size(*root) + 1;
+  parent = *root;
+
+  mask = 1 << (31 - __builtin_clz(size) - 1);
+
+    while (mask > 1)
+    {
+        if (size & mask)
+            parent = parent->right;
+        else
+            parent = parent->left;
+        mask >>= 1;
+    }
+
+    new_node = binary_tree_node(parent, value);
+    if (!new_node)
+        return (NULL);
+
+    if (size & 1)
+        parent->right = new_node;
+    else
+        parent->left = new_node;
+
+    while (new_node->parent && new_node->n > new_node->parent->n)
+    {
+        temp = new_node->n;
+        new_node->n = new_node->parent->n;
+        new_node->parent->n = temp;
+        new_node = new_node->parent;
+    }
+
+    return (new_node);
+}
--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -26,5 +26,6 @@ typedef struct binary_tree_s binary_tree_t;
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
 
 #endif /*BINARY_TREES_H*/
