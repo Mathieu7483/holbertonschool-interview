7eb45d6545d283d45b3b005daeca171332b3b030
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-17 09:38:19 +0200

    style: format comments for consistency and readability in heap_insert.c

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 
-/**binary_tree_size - function that calculate the size of the tree
+/**
+ * binary_tree_size - function that calculate the size of the tree
  * @tree: is a pointer to the root node of the tree to measure the size
  * Return: size of the tree
  */
@@ -11,13 +12,12 @@ size_t binary_tree_size(const binary_tree_t *tree)
   return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
 
-
 /**
-*heap_insert - function that inserts a value in Max Binary Heap
-*@root: is a double pointer to the root node of the Heap
-*@value: is the value to store in the node to be inserted
-*Return: pointer to the created node, or NULL on failure
-*/
+ * heap_insert - function that inserts a value in Max Binary Heap
+ * @root: is a double pointer to the root node of the Heap
+ * @value: is the value to store in the node to be inserted
+ * Return: pointer to the created node, or NULL on failure
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
   heap_t *new_node, *parent;
@@ -34,31 +34,31 @@ heap_t *heap_insert(heap_t **root, int value)
 
   mask = 1 << (31 - __builtin_clz(size) - 1);
 
-    while (mask > 1)
-    {
-        if (size & mask)
-            parent = parent->right;
-        else
-            parent = parent->left;
-        mask >>= 1;
-    }
-
-    new_node = binary_tree_node(parent, value);
-    if (!new_node)
-        return (NULL);
-
-    if (size & 1)
-        parent->right = new_node;
+  while (mask > 1)
+  {
+    if (size & mask)
+      parent = parent->right;
     else
-        parent->left = new_node;
+      parent = parent->left;
+    mask >>= 1;
+  }
 
-    while (new_node->parent && new_node->n > new_node->parent->n)
-    {
-        temp = new_node->n;
-        new_node->n = new_node->parent->n;
-        new_node->parent->n = temp;
-        new_node = new_node->parent;
-    }
+  new_node = binary_tree_node(parent, value);
+  if (!new_node)
+    return (NULL);
 
-    return (new_node);
+  if (size & 1)
+    parent->right = new_node;
+  else
+    parent->left = new_node;
+
+  while (new_node->parent && new_node->n > new_node->parent->n)
+  {
+    temp = new_node->n;
+    new_node->n = new_node->parent->n;
+    new_node->parent->n = temp;
+    new_node = new_node->parent;
+  }
+
+  return (new_node);
 }
