f9e81e5e759a8d66b1d7aa282e669f6f0c18b639
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-17 08:50:19 +0200

    feat: add binary tree structure and main function for testing

---
+++ heap_insert/binary_trees.h
@@ -0,0 +1,22 @@
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s heap_t;
+
+/* Function prototypes */
+heap_t *binary_tree_node(heap_t *parent, int value);
+void binary_tree_print(const heap_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
