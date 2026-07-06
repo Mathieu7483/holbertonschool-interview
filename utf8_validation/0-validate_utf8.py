4c503d238cfaf7039fe0176f0af1df51d944ce5e
Mathieu7483 <mathieu.godalier@orange.fr>
2026-07-06 12:45:49 +0200

    feat: add UTF-8 validation functionality with validUTF8 method and test cases

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Write a method that determines if a given data
+set represents a valid UTF-8 encoding."""
+
+
+def validUTF8(data):
+    """Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (list): A list of integers representing the data set.
+
+    Returns:
+        bool: True if the data set is a valid UTF-8 encoding, False otherwise.
+    """
+    num_bytes = 0
+
+    for byte in data:
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7):
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
