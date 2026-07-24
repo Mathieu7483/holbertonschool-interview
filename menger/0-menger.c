cf5a0d45e118cfb0a1c945080c461435668396f4
Mathieu7483 <mathieu.godalier@orange.fr>
2026-07-24 09:28:18 +0200

    feat: refactor is_empty function and improve menger function documentation

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -1,29 +1,47 @@
 #include "menger.h"
 
+/**
+ * is_empty - checks if a given position in the Menger Sponge is empty
+ * row: the row index of the position
+ * col: the column index of the position
+ * 
+ * Return: 1 if the position is empty, 0 otherwise
+ */
+
+static int is_empty(int row, int col)
+{
+		while (row > 0 || col > 0)
+		{
+				if (row % 3 == 1 && col % 3 == 1)
+						return (1);
+				row /= 3;
+				col /= 3;
+		}
+		return (0);
+}
+
 /*
-* void menger(int level);
-* int level: level of the Menger Sponge to draw
+* void menger(int level)- draws a Menger Sponge of a given level
+* @level: level of the Menger Sponge to draw
 */
 
 void menger(int level)
 {
-	if (level < 0)
-		return;
+		int size, row, col;
 
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
+		if (level < 0)
+			return;
+
+		size = pow(3, level);
+		for (row = 0; row < size; row++)
 		{
-			if (i == 1 && j == 1)
+			for (col = 0; col < size; col++)
 			{
-				for (int k = 0; k < level; k++)
-					printf("   ");
+				if (is_empty(row, col))
+					printf(" ");
+				else
+					printf("#");
 			}
-			else
-			{
-				menger(level - 1);
-			}
+			printf("\n");
 		}
-		printf("\n");
-	}
 }
