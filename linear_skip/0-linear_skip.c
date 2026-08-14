3b2451dffeb525f4e803f53a740b40e0b2e94417
Mathieu7483 <mathieu.godalier@orange.fr>
2026-08-14 09:12:22 +0200

    refactor: improve linear_skip function structure and readability

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "search.h"
 
@@ -7,42 +8,46 @@
  * @head: Pointer to the head of the list
  * @value: Value to search for
  *
- * Return: Pointer to the first node where value is located, or NULL if not found
+ * Return: Pointer to the first node where value is located, or NULL if not
  */
 skiplist_t *linear_skip(skiplist_t *head, int value)
 {
-  skiplist_t *node, *prev;
+	skiplist_t *node, *prev;
 
-  if (!head)
-    return (NULL);
-
-  for (node = head; node->express && node->express->n < value; node = node->express)
-    {
-      printf("Value checked at index [%lu] = [%d]\n", node->express->index, node->express->n);
-    }
-
-  prev = node;
-  if (node->express)
-    {
-      node = node->express;
-      printf("Value found between indexes [%lu] and [%lu]\n", prev->index, node->index);
-    }
-  else
-    {
-      while (node->next)
-  {
-    node = node->next;
-  }
-      printf("Value found between indexes [%lu] and [%lu]\n", prev->index, node->index);
-    }
-
-  for (node = prev; node && node->n < value; node = node->next)
-    {
-      printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
-    }
-
-  if (node && node->n == value)
-    return (node);
-
-  return (NULL);
+	if (!head)
+		return (NULL);
+	node = head;
+	/* 1. Parcours de l'express lane */
+	while (node->express)
+	{
+		prev = node;
+		node = node->express;
+		printf("Value checked at index [%lu] = [%d]\n",
+		       node->index, node->n);
+		if (node->n >= value)
+			break;
+	}
+	/* 2. Si on est arrivés au bout de l'express lane sans dépasser la valeur */
+	if (!node->express && node->n < value)
+	{
+		prev = node;
+		while (node->next)
+			node = node->next;
+	}
+	/* 3. Affichage de la plage de recherche */
+	printf("Value found between indexes [%lu] and [%lu]\n",
+	       prev->index, node->index);
+	/* 4. Parcours linéaire classique */
+	node = prev;
+	while (node)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       node->index, node->n);
+		if (node->n == value)
+			return (node);
+		if (node->n > value)
+			return (NULL);
+		node = node->next;
+	}
+	return (NULL);
 }
