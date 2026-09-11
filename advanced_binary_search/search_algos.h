2258d9ad0bfe0f3076e48acf2d9d276183e8cc20
Mathieu7483 <mathieu.godalier@orange.fr>
2026-09-11 09:31:48 +0200

    feat: add header file for advanced binary search functions

---
+++ advanced_binary_search/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef ALGOS_SEARCH_H
+#define ALGOS_SEARCH_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* ALGOS_SEARCH_H */
