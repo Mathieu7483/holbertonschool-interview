f79426f761532a2f7910f866aeb270ac49146b23
Mathieu7483 <mathieu.godalier@orange.fr>
2026-09-11 09:31:40 +0200

    feat: implement advanced binary search algorithm with array printing function

---
+++ advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,58 @@
+#include "search_algos.h"
+
+/**
+ * print_array - Prints an array of integers
+ * @array: Pointer to the first element of the array
+ * @low: Starting index of the subarray
+ * @high: Ending index of the subarray
+ */
+void print_array(int *array, size_t low, size_t high)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = low; i <= high; i++)
+	{
+		printf("%d", array[i]);
+		if (i < high)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+*advanced_binary - function that creates a search for a value in a sorted array
+*@array: is a pointer to the first element of the array to search in
+*@size: is the number of elements in array 
+*@value: is the value to search for
+*Return: the index of the value
+*/
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t mid;
+	int result;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	print_array(array, size);
+
+	if (size == 1)
+	{
+		if (array[0] == value)
+			return (0);
+		return (-1);
+	}
+
+	mid = (size - 1) / 2;
+
+	if (array[mid] >= value)
+		return (advanced_binary(array, mid + 1, value));
+
+	result = advanced_binary(array + mid + 1, size - mid - 1, value);
+	if (result != -1)
+		return (result + (int)mid + 1);
+
+	return (-1);
+}
