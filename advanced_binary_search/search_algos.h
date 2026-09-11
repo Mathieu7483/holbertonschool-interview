a9af9dbfe68d488362c3abc2e9bd8c6d6f970cdd
Mathieu7483 <mathieu.godalier@orange.fr>
2026-09-11 10:24:49 +0200

    fix: correct header guard and remove unused include in search_algos.h

--- advanced_binary_search/search_algos.h
+++ advanced_binary_search/search_algos.h
@@ -1,10 +1,11 @@
-#ifndef ALGOS_SEARCH_H
-#define ALGOS_SEARCH_H
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main(void);
+void print_array(int *array, size_t low, size_t high);
+int recursive_advanced_binary(int *array, size_t low, size_t high, int value);
 int advanced_binary(int *array, size_t size, int value);
 
-#endif /* ALGOS_SEARCH_H */
+#endif /* SEARCH_ALGOS_H */
