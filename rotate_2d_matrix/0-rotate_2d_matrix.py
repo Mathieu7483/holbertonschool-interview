7b33e54f4e083adac006dd2497b952c9062c6808
Mathieu7483 <mathieu.godalier@orange.fr>
2026-09-18 10:54:22 +0200

    style: add blank lines for improved readability in rotate_2d_matrix.py

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Given an n x n 2D matrix, rotate it 90 degrees clockwise in-place."""
+
+
 def rotate_2d_matrix(matrix):
     """Rotate a 2D matrix 90 degrees clockwise in-place."""
     n = len(matrix)
